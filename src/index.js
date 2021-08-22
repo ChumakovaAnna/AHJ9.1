4153cfe40003c832bbb5a96cf7afb19faa43ed2e
Anna Chumakova <chumakova3008@gmail.com>
2021-08-23 01:05:18 +0500

    created button and collapse

--- src/index.js
+++ src/index.js
@@ -1,8 +1,2 @@
 import "./css/style.css";
 import "./js/app";
-
-import "./img/delete.png";
-import "./img/add.png";
-import "./img/edit.png";
-import "./img/cancel.png";
-import "./img/submit.png";
