9d64e1c5b3562ab54dc64683bab6c33ea3d2aa7d
Anna Chumakova <chumakova3008@gmail.com>
2021-08-27 01:31:32 +0500

    подчищаем код по eslint

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,9 @@
 function collapse() {
-  const button = document.querySelector(".button-collapse");
-  const eleCollapse = document.querySelector(".collapse");
+  const button = document.querySelector('.button-collapse');
+  const eleCollapse = document.querySelector('.collapse');
 
-  button.addEventListener("click", () => {
-    eleCollapse.classList.toggle("active");
+  button.addEventListener('click', () => {
+    eleCollapse.classList.toggle('active');
   });
 }
 
